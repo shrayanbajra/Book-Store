cc4c1faf2ded3766e6b7ed774bc35e5cdc5ad7f3
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-04 22:54:37 +0545

    Removed SecondFragment

--- app/src/main/java/com/example/bookstore/FirstFragment.kt
+++ app/src/main/java/com/example/bookstore/FirstFragment.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
@@ -24,8 +19,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 }
