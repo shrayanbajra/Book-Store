d8c9d55e746424ef635c503acd4f13cc2ec06368
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-05 20:56:40 +0545

    Added remaining genres to the RecyclerView

--- app/src/main/java/com/example/bookstore/CatalogFragment.kt
+++ app/src/main/java/com/example/bookstore/CatalogFragment.kt
@@ -44,7 +44,24 @@ class CatalogFragment : Fragment() {
 
     private fun getGenres(): List<Genre> {
         val action = Genre(imageResource = R.drawable.ic_car_crash, title = "Action")
-        val biography = Genre(imageResource = R.drawable.ic_vaadin_glasses, title = "Biography")
-        return listOf(action, biography)
+        val biography = Genre(imageResource = R.drawable.ic_glasses, title = "Biography")
+        val cookbook = Genre(imageResource = R.drawable.ic_apple, title = "Cookbook")
+        val crime = Genre(imageResource = R.drawable.ic_knife, title = "Crime")
+        val drama = Genre(imageResource = R.drawable.ic_theater_masks, title = "Drama")
+        val fantasy = Genre(imageResource = R.drawable.ic_magic, title = "Fantasy")
+        val guide = Genre(imageResource = R.drawable.ic_map_signs, title = "Guide")
+        val health = Genre(imageResource = R.drawable.ic_health, title = "Health")
+        val horror = Genre(imageResource = R.drawable.ic_grave, title = "Horror")
+        val poetry = Genre(imageResource = R.drawable.ic_pen_nib, title = "Poetry")
+        val religion = Genre(imageResource = R.drawable.ic_praying_hands, title = "Religion")
+        val romance = Genre(imageResource = R.drawable.ic_heart, title = "Romance")
+        val science = Genre(imageResource = R.drawable.ic_genius, title = "Science")
+        val scienceFiction = Genre(imageResource = R.drawable.ic_alien, title = "Science fiction")
+        val thriller = Genre(imageResource = R.drawable.ic_skull, title = "Thriller")
+        val travel = Genre(imageResource = R.drawable.ic_airplane, title = "Travel")
+        return listOf(
+            action, biography, cookbook, crime, drama, fantasy, guide, health, horror, poetry,
+            religion, romance, science, scienceFiction, thriller, travel
+        )
     }
 }
