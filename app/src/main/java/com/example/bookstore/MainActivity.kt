26b633d136c431df56d20e9459577a03c46db785
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-06 20:33:47 +0545

    Setup NavController with BottomNavView

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.bookstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomNavView = findViewById(R.id.bottom_navigation)
+        setupNavControllerWithBottomNavView()
+
+    }
+
+    private fun setupNavControllerWithBottomNavView() {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        bottomNavView.setupWithNavController(navController)
     }
 
 }
